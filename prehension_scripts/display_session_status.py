b13f5fab3604d84df30d77115ec19e3aa4dc5505
Caleb Raman <craman96@gmail.com>
2024-11-05 08:42:06 -0600

    re-add prehension_scripts

---
+++ prehension_scripts/display_session_status.py
@@ -0,0 +1,93 @@
+#!python3
+# -*- coding: utf-8 -*-
+"""
+script for displaying status of prehension analyses across presets and sessions
+
+Copyright (C) 2019-2024 Anton Sobinov, Caleb Raman
+https://github.com/BensmaiaLab/prehension
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
+import sys
+import argparse
+
+from prehension.visualization import session_status_visualization
+from prehension_presets.prehension_presets import PRESETS
+from prehension.tools.session_management import fetch_server_session_dirs
+from prehension.visualization.session_data_visualization import SessionWrapper
+from colorama import init
+
+
+preset_names = [
+    "daiquiri_right_hemisphere",
+    "pimms_left_hemisphere_training_k1",
+    "pappy_left_hemisphere_training_v1",
+]
+
+
+def main(args):
+    """
+    Helper script for displaying sessions (raw & processed) in experiment and training groups
+    across the presets in preset names. Option to transfer sessions to training server if
+    --clean is passed.
+    """
+    for preset_name in preset_names:
+        print(preset_name.upper())
+        init(autoreset=True)  # for colorama
+
+        if preset_name not in PRESETS.keys():
+            raise ValueError(
+                f"preset_name {preset_name} not found in presets {list(PRESETS.keys())}"
+            )
+
+        current_preset = PRESETS[preset_name]
+        # Get raw/processed session dirs for preset
+        experimental_ss_pairs, training_ss_pairs = fetch_server_session_dirs(
+            current_preset, sessions=[], filter=False
+        )
+
+        exp_session_wrappers = [SessionWrapper(*exp_pair) for exp_pair in experimental_ss_pairs]
+        train_session_wrappers = [SessionWrapper(*train_pair) for train_pair in training_ss_pairs]
+
+        session_status_visualization.display_session_info(
+            exp_session_wrappers, train_session_wrappers, args.clean, args.last
+        )
+        print()
+        print()
+
+
+# Entry
+if __name__ == "__main__":
+
+    # Add arguments
+    parser = argparse.ArgumentParser(description=("Display session info about a given monkey"))
+
+    parser.add_argument(
+        "--clean",
+        action="store_true",
+        help=(
+            "Remove session folders if the log file is found somewhere else and there is"
+            " no sensor data in raw"
+        ),
+        default=False,
+    )
+
+    parser.add_argument(
+        "--last", type=int, default=-1, help=("Number of sessions to display. Default: -1 (all)")
+    )
+
+    args = parser.parse_args(sys.argv[1:])
+    main(args)
--- prehension_scripts/export_digit_forces.py
+++ prehension_scripts/export_digit_forces.py
@@ -1,4 +1,5 @@
-#!python3.7
+#!python3
+# -*- coding: utf-8 -*-
 import argparse
 import datetime
 import time
@@ -10,22 +11,23 @@ from prehension import tools
 from prehension.matching.export_digit_forces import export_digit_forces
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     current_preset_name, current_preset, argv = preset.process_args_for_preset()
 
     parser = argparse.ArgumentParser(
-        description=('Compare manually-labeled to the automatically-labeled forces using sensor'
-                     ' masks.'))
-    tools.add_default_kwarguments(
-        parser, {'server': current_preset['default_server']})
-    tools.add_default_arguments(
-        parser, ('sessions', 'trials', 'temp', 'overwrite', 'processes'))
+        description=(
+            "Compare manually-labeled to the automatically-labeled forces using sensor" " masks."
+        )
+    )
+    tools.add_default_kwarguments(parser, {"server": current_preset["default_server"]})
+    tools.add_default_arguments(parser, ("sessions", "trials", "temp", "overwrite", "processes"))
 
     args = parser.parse_args(args=argv)
 
     start_time = time.time()
-    export_digit_forces(args.server, args.sessions, args.trials, args.temp, args.overwrite, args.processes)
-    print('Program took {}.'.format(
-        datetime.timedelta(seconds=time.time() - start_time)))
+    export_digit_forces(
+        args.server, args.sessions, args.trials, args.temp, args.overwrite, args.processes
+    )
+    print("Program took {}.".format(datetime.timedelta(seconds=time.time() - start_time)))
 
     plt.show()
