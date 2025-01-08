4885c76e99fd678a617644efc0d5a10311fbdee2
Anton Sobinov <an.sobinov@gmail.com>
2025-01-08 10:31:10 -0600

    proc status defs

--- prehension_scripts/display_processing_status.py
+++ prehension_scripts/display_processing_status.py
@@ -35,8 +35,8 @@ from prehension.visualization.session_data_visualization import SessionWrapper
 
 
 PRESET_NAMES = ['mojito_right_hemisphere']
-# PRESET_NAMES = ['pimms_right_hemisphere']
-# PRESET_NAMES = ['daiquiri_right_hemisphere']
+PRESET_NAMES = ['pimms_right_hemisphere']
+PRESET_NAMES = ['daiquiri_right_hemisphere']
 
 
 def main(args):
